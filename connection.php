0e1ea80b7edac62e005728e5a9dac4a4d4d6ddd8
IntanLarasati <74240360+IntanLarasati@users.noreply.github.com>
2020-11-11 13:57:00 +0800

    connect.php
    membuat koneksi antara server dan data

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$host = mysqli_connect("localhost","root","","restu_adi_travel");
+if($host){
+    echo 'success';
+} else echo 'failed';
+?>
