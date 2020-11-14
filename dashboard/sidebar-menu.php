2f81c02389c74805395f5144beb49c4ee4eb6e7a
Eka Laksmana <ekalaksmana@users.noreply.github.com>
2020-11-14 19:18:35 +0800

    Basic Installing The theme dashboard

---
+++ dashboard/sidebar-menu.php
@@ -0,0 +1,28 @@
+<ul class="vertical-nav-menu">
+    <li class="app-sidebar__heading">Dashboards</li>
+    <li>
+        <a href="index.php" class="">
+            <i class="metismenu-icon pe-7s-rocket"></i>
+            Halaman Utama Ku    
+        </a>
+    </li>
+    <li class="app-sidebar__heading">Data Reservasi</li>
+    <li>
+        <a href="request-reservasi.php">
+            <i class="metismenu-icon pe-7s-mail"></i>
+            Data Request Reservasi
+        </a>
+    </li>
+    <li>
+        <a href="confirm-reservasi.php">
+            <i class="metismenu-icon pe-7s-call"></i>
+            Data sudah Konfirmasi
+        </a>
+    </li>
+    <li>
+        <a href="cancel-booked.php">
+            <i class="metismenu-icon pe-7s-close-circle"></i>
+            Data Cancel booked
+        </a>
+    </li>            
+</ul>
---
+++ dashboard/widget-akun.php
@@ -0,0 +1,31 @@
+<div class="header-btn-lg pr-0">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left">
+                                    <div class="btn-group">
+                                        <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="p-0 btn">
+                                            <img width="42" class="rounded-circle" src="assets/images/avatars/1.jpg" alt="">
+                                            <i class="fa fa-angle-down ml-2 opacity-8"></i>
+                                        </a>
+                                        <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
+                                            <button type="button" tabindex="0" class="dropdown-item">Logout</button>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="widget-content-left  ml-3 header-user-info">
+                                    <div class="widget-heading">
+                                        Admin Restu adi travel
+                                    </div>
+                                    <div class="widget-subheading">
+                                        Data management
+                                    </div>
+                                </div>
+                                <div class="widget-content-right header-user-info ml-3">
+                                    <button type="button" class="btn-shadow p-1 btn btn-primary btn-sm show-toastr-example">
+                                        <i class="fa text-white fa-calendar pr-1 pl-1"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
